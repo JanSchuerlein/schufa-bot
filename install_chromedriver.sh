c9ef2174f7d1d92127e96c379dbe760acd24b5f4
Jan Schürlein <me@janomine.de>
2024-04-27 19:21:02 +0200

    Changed zip file name

--- install_chromedriver.sh
+++ install_chromedriver.sh
@@ -15,8 +15,8 @@ rm ./google-chrome-stable_current_amd64.deb
 printf '\n\n\nDownloading and installing the latest chromedriver version 114.* (v. 04/2024)...\n\n\n'
 
 wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chromedriver-linux64.zip
-unzip chromedriver_linux64.zip
+unzip chromedriver-linux64.zip
 mv ./chromedriver /usr/bin/chromedriver
 rm ./LICENSE.chromedriver
 chmod +x /usr/bin/chromedriver
-rm -r chromedriver_linux64.zip
+rm -r chromedriver-linux64.zip
